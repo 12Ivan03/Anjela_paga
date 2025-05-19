ece7a9bb61d6d4e12a00ef7a44fe33d0d7e01f3f
12Ivan03 <144702214+12Ivan03@users.noreply.github.com>
2025-05-19 18:19:44 +0200

    5concerts added

---
+++ js/concert_sccript.js
@@ -0,0 +1,37 @@
+// Copy nav bar no Language buttons
+
+const navbar = document.getElementById("nav_bar");
+const cloneNavbar = navbar.cloneNode(true);
+const cpyNoLang = cloneNavbar.querySelector("#language");
+if (cpyNoLang) {
+	cpyNoLang.remove();
+}
+cloneNavbar.id = "nav_bar_clone";
+document.getElementById("footer_nav_part").appendChild(cloneNavbar);
+
+// language button
+const bgLang = document.querySelector('.bg_lang');
+const enLang = document.querySelector('.en_lang');
+
+bgLang.addEventListener('click', () => {
+	bgLang.classList.toggle('act');
+
+	if (!bgLang.classList.contains('act')) {
+		bgLang.classList.add('act');
+	}
+	if (enLang.classList.contains('act')) {
+		enLang.classList.remove('act');
+	}
+});
+
+enLang.addEventListener('click', () => {
+	enLang.classList.toggle('act');
+
+	if (!enLang.classList.contains('act')) {
+		enLang.classList.add('act');
+	}
+	if (bgLang.classList.contains('act')) {
+		bgLang.classList.remove('act');
+	}
+}
+);
--- js/script.js
+++ js/script.js
@@ -86,9 +86,13 @@ function updateCarousel() {
 	carousel.style.transform = `translateX(${offset}px)`;
 }
 
-// Copy nav bar
+// Copy nav bar no Language buttons
 
 const navbar = document.getElementById("nav_bar");
 const cloneNavbar = navbar.cloneNode(true);
+const cpyNoLang = cloneNavbar.querySelector("#language");
+if (cpyNoLang) {
+	cpyNoLang.remove();
+}
 cloneNavbar.id = "nav_bar_clone";
 document.getElementById("footer_nav").appendChild(cloneNavbar);
