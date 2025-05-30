a06940c675e235b1db6dcd2747689ef3717a9dac
12Ivan03 <144702214+12Ivan03@users.noreply.github.com>
2025-05-30 22:19:07 +0200

    clean up

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-// nav bar button menu
-
-// const button = document.getElementById("changeBtn");
-// const dropDown = document.getElementById("dropMenu");
-
-// button.addEventListener("click", function () {
-// 	// document.getElementById("message").textContent = "You clicked the button!";
-// 	if (dropDown.style.display === "none"){
-// 		dropDown.style.display = "block";
-// 	} else {
-// 		dropDown.style.display = "none";
-// 	}
-// });
 
 // language button
 const bgLang = document.querySelector('.bg_lang');
